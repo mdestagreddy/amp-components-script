0d205a0068d76d0239e80be387ff34aeb54ebd9f
mdestagreddy <67367967+mdestagreddy@users.noreply.github.com>
2021-08-17 08:25:35 +0800

    init.js
    AMPComponents() for AMP ⚡

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 function AMPComponents() {
   var loaded = "Successful loaded component: _s";
   var error = "Cannot load component: _s";
+  var isInit = false;
   this.init = function() {
+    isInit = true;
     var _main = document.createElement("script");
     document.head.appendChild(_main);
     _main.setAttribute("async", "");
     _main.src = "https://cdn.ampproject.org/v0.js";
   }
   this.component = function(componentName, version) {
-    var scripts = document.createElement("script");
-    document.head.appendChild(scripts);
-    scripts.setAttribute("async", "");
-    scripts.setAttribute("custom-element", "amp-" + componentName);
-    scripts.src = "https://cdn.ampproject.org/v0/amp-" + componentName + "-" + version + ".js";
-    scripts.addEventListener("load", function() {
-      console.log(loaded.replace("_s", "amp-" + componentName + "-" + version + ".js"));
-    });
-    scripts.addEventListener("error", function() {
-      this.remove();
-      var err = error.replace("_s", "amp-" + componentsName + "-" + version + ".js");
-      console.error(err);
-      throw TypeError(err);
-    });
+    if (isInit) {
+      var scripts = document.createElement("script");
+      document.head.appendChild(scripts);
+      scripts.setAttribute("async", "");
+      scripts.setAttribute("custom-element", "amp-" + componentName);
+      scripts.src = "https://cdn.ampproject.org/v0/amp-" + componentName + "-" + version + ".js";
+      scripts.addEventListener("load", function() {
+        console.log(loaded.replace("_s", "amp-" + componentName + "-" + version + ".js"));
+      });
+      scripts.addEventListener("error", function() {
+        this.remove();
+        var err = error.replace("_s", "amp-" + componentsName + "-" + version + ".js");
+        console.error(err);
+        throw TypeError(err);
+      });
+    }
+    else {
+      var errInit = "Your need .init() function instead. [amp-" + componentName + "-" + version + ".js]";
+      console.error(errInit);
+      throw Error(errInit);
+    }
   }
 }
